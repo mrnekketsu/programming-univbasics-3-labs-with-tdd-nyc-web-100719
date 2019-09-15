ce19d435941330fed79f95a8fdd0f6ca40593961
Nick <nick.moi@gmail.com>
2019-09-15 03:32:42 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,4 +3,6 @@ first_number = 0
 second_number = 1
 
 sum = first_number + second_number
-difference = 
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
